74a805a8d0c94b15e77269bbaf7b07b284049e6c
Joe Labi <mwbetrg@gmail.com>
2015-07-21 00:03:27 +0000

    Adding ; at the end of line

--- query-iotd.sql
+++ query-iotd.sql
@@ -1,1 +1,1 @@
-select "\textbf{\so{"||idiom||"}} \bigskip " ||  "\textit{"||meaning||"} \bigskip " || "\texttt{"||sentence||"} " as note  from iotd where date = "20150721"
+select "\textbf{\so{"||idiom||"}} \bigskip " ||  "\textit{"||meaning||"} \bigskip " || "\texttt{"||sentence||"} " as note  from iotd where date = "20150721" ;
--- query-wotd.sql
+++ query-wotd.sql
@@ -1,1 +1,1 @@
-select "\textbf{\so{"||word||"}} \medskip " ||  "\begin{minipage}{14cm}\textcolor{magenta}{["|| part ||"]}" || " \textit{"||meaning||"} \end{minipage} \bigskip " || "\begin{minipage}{14cm}\texttt{"||sentence||"} \end{minipage}" as note  from wotd where date = "20150721"
+select "\textbf{\so{"||word||"}} \medskip " ||  "\begin{minipage}{14cm}\textcolor{magenta}{["|| part ||"]}" || " \textit{"||meaning||"} \end{minipage} \bigskip " || "\begin{minipage}{14cm}\texttt{"||sentence||"} \end{minipage}" as note  from wotd where date = "20150721" ;
