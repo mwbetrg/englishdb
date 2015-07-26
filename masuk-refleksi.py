6d6af6d42efaca6edf73ea8f1d3de4fed520944a
Joe Labi <mwbetrg@gmail.com>
2015-07-26 11:33:49 +0000

    Refleksi

---
+++ masuk-refleksi.py
@@ -0,0 +1,32 @@
+#qpy:2
+#qpy:console
+
+import site
+from peewee import *
+import datetime
+
+database = SqliteDatabase('lessonplan2010.db', **{})
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Reflections(BaseModel):
+    kelas = CharField(null=True)
+    note = CharField(null=True)
+    time = DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'reflections'
+
+
+masa = datetime.datetime.today()
+
+refleksi = raw_input("\nMasukkan refleksi: \n")
+
+ting = raw_input("\nMasukkan kelas: \n")
+
+rekod = Reflections.insert(note=refleksi, kelas=ting, time=masa).execute()
+
+print refleksi+"\n"+" : "+ting+" : "+str(masa)
