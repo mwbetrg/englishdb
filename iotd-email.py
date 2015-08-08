a4be4466ffd28915e2aa00564e8185ee0fc35de1
Joe Labi <mwbetrg@gmail.com>
2015-08-08 21:05:14 +0800

    Adding python email for wotd and iotd

--- iotd-email.py
+++ iotd-email.py
@@ -1,8 +1,21 @@
 import smtplib
+import datetime
+import time
+
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
+
+today = datetime.datetime.today()
+adaybefore = today - datetime.timedelta(days=1)
+tarikh = (time.strftime("%Y%m%d"))
+semalam = adaybefore.strftime("%Y-%m-%d")
+
+sdir = "/storage/extSdCard/texdocs/iotd/"
+filename = "iotd-"+tarikh+".pdf.jpg" 
+fullpathfilename = sdir+"iotd-"+tarikh+".pdf.jpg"    
+
 fromaddr = "mwbetrg@gmail.com"
 toaddr = "nege725saze@post.wordpress.com"
 msg = MIMEMultipart ()
@@ -12,16 +25,16 @@ msg['Subject' ] = "Idiom Of The Day"
 body = "[category idiom] \n\
 [tags iotd] \n\
 [status publish] \n\
-[delay 2015-08-07 22:20:00] \n\
+[delay "+semalam+" 22:10:00] \n\
 [end]"
 
+
 msg.attach(MIMEText(body, 'plain' ))
-filename = "iotd-20150808.pdf.jpg"
-attachment = open("/storage/extSdCard/texdocs/iotd/iotd-20150808.pdf.jpg", "rb")
+attachment = open(filename , "rb")
 part = MIMEBase('application' , 'octet-stream' )
 part.set_payload ((attachment).read())
-encoders.encode_base64 (part)
-part.add_header('Content-Disposition' , "attachment; filename= %s" % filename)
+encoders.encode_base64(part)
+part.add_header('Content-Disposition' , "attachment; filename= %s" % fullpathfilename)
 msg.attach(part)
 server = smtplib.SMTP('smtp.gmail.com' ,587 )
 server.starttls()
--- wotd-email.py
+++ wotd-email.py
@@ -1,8 +1,21 @@
 import smtplib
+import datetime
+import time
+
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEBase import MIMEBase
 from email import encoders
+
+today = datetime.datetime.today()
+adaybefore = today - datetime.timedelta(days=1)
+tarikh = (time.strftime("%Y%m%d"))
+semalam = adaybefore.strftime("%Y-%m-%d")
+
+sdir = "/storage/extSdCard/texdocs/wotd/"
+filename = "wotd-"+tarikh+".pdf.jpg" 
+fullpathfilename = sdir+"wotd-"+tarikh+".pdf.jpg"    
+
 fromaddr = "mwbetrg@gmail.com"
 toaddr = "nege725saze@post.wordpress.com"
 msg = MIMEMultipart ()
@@ -12,16 +25,16 @@ msg['Subject' ] = "Word Of The Day"
 body = "[category word] \n\
 [tags wotd] \n\
 [status publish] \n\
-[delay 2015-08-07 22:00:00] \n\
+[delay "+semalam+" 22:00:00] \n\
 [end]"
 
+
 msg.attach(MIMEText(body, 'plain' ))
-filename = "wotd-20150808.pdf.jpg"
-attachment = open("/storage/extSdCard/texdocs/wotd/wotd-20150807.pdf.jpg", "rb")
+attachment = open(filename , "rb")
 part = MIMEBase('application' , 'octet-stream' )
 part.set_payload ((attachment).read())
-encoders.encode_base64 (part)
-part.add_header('Content-Disposition' , "attachment; filename= %s" % filename)
+encoders.encode_base64(part)
+part.add_header('Content-Disposition' , "attachment; filename= %s" % fullpathfilename)
 msg.attach(part)
 server = smtplib.SMTP('smtp.gmail.com' ,587 )
 server.starttls()
